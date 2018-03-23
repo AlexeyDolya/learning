2226167f52d898d6aed50a293bce79e61b123e39
AlexeyDolya <alexeyjezzadolya@gmail.com>
2018-03-23 12:50:47 +0200

    fix versions, correct code

--- src/test/Loops/evenNum.js
+++ src/test/Loops/evenNum.js
@@ -1,11 +1,11 @@
 export function evenNum(i, a) {
   let arr = [];
   for (i = 0; i < a; i++) {
-    if (i % 2 !== 0) continue;
-    {
-      arr.push(i);
+    if (i % 2 !== 0) {
+      continue;
     }
+    arr.push(i);
   }
   return arr;
 }
-+evenNum(0, 10);
+evenNum(0, 10);
